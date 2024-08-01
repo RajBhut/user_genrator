f7ce4a8c89dd01769bd0bf027941e5564b37eba7
Raj Bhut <rajbhut2832005@gmail.com>
2024-08-01 10:42:43 +0530

    v1

---
+++ index.js
@@ -0,0 +1,73 @@
+import mongoose from 'mongoose';
+import faker from 'faker';
+
+
+mongoose.connect('mongodb://localhost:27017/w3s'
+ );
+
+
+const userSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+
+  address: {
+    street: String,
+    city: String,
+    state: String,
+    zipCode: String
+  },
+  phoneNumbers: [{
+    type: {
+      type: String,
+      enum: ['mobile', 'home', 'work'] 
+    },
+    number: String
+  }],
+  
+
+  hobbies: [String],
+  socialMedia: {
+    facebook: String,
+    twitter: String,
+    instagram: String
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+
+const generateDummyUser = () => {
+  return {
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+   
+    address: {
+      street: faker.address.streetAddress(),
+      city: faker.address.city(),
+      state: faker.address.state(),
+      zipCode: faker.address.zipCode()
+    },
+    phoneNumbers: [
+      { type: 'mobile', number: faker.phone.phoneNumber() }
+    ],
+    hobbies: [...faker.lorem.words()],
+    socialMedia: {
+      facebook: faker.internet.userName(),
+      twitter: `@${faker.internet.userName()}`,
+      instagram: faker.internet.userName()
+    }
+  };
+};
+
+git reset
+
+const createDummyUsers = async (count) => {
+  const users = [];
+  for (let i = 0; i < count; i++) {
+    users.push(generateDummyUser());
+  }
+  await User.insertMany(users);
+};
+export default createDummyUsers;
+
+
